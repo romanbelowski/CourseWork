163eb80c3aca580b2f1d3777cb7fc28656330840
romanbelowski <roman_belowski@protonmail.com>
2023-12-12 09:39:15 -0800

    Add project files.

---
+++ ShoppingCart/Infrastructure/Components/CategoriesViewComponent.cs
@@ -0,0 +1,18 @@
+﻿using CourseWork.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseWork.Infrastructure.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly DataContext _context;
+
+        public CategoriesViewComponent(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _context.Categories.ToListAsync());
+    }
+}
---
+++ ShoppingCart/Infrastructure/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using CourseWork.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseWork.Infrastructure
+{
+    public class DataContext : IdentityDbContext<AppUser>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        { }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
