c06ece84f83ef4dcb148ccd3b29b7d9c7488b140
romanbelowski <roman.belowsky@gmail.com>
2024-10-04 19:49:39 +0300

    Add: Simple logging features

--- ShoppingCart/Program.cs
+++ ShoppingCart/Program.cs
@@ -3,15 +3,24 @@ using CourseWork.Models;
 using CourseWork.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Events;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.File("Logs/log.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
 
 // Add DbContext to the container.
 builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddDistributedMemoryCache();
-
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromMinutes(30);
@@ -19,11 +28,12 @@ builder.Services.AddSession(options =>
 });
 
 ConfigureIdentity(builder.Services);
-
 builder.Services.AddScoped<PasswordValidationService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
 
 var app = builder.Build();
 
@@ -33,31 +43,38 @@ app.UseSession();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.Use((context, next) =>
+{
+    var request = context.Request;
+    var ipAddress = context.Connection.RemoteIpAddress;
+    var requestTime = DateTime.Now;
+    var logMessage = $"{request.Scheme}://{request.Host}{request.Path}{request.QueryString} [:] " +
+        $"T:{requestTime}, " +
+        $"IP:{ipAddress}";
+    Log.Information(logMessage);
+    return next();
+});
+
 
 app.MapControllerRoute(
     name: "Areas",
     pattern: "{area:exists}/{controller=Products}/{action=Index}/{id?}");
-
 app.MapControllerRoute(
     name: "products",
     pattern: "/products/{categorySlug?}",
     defaults: new { controller = "Products", action = "Index" });
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 void ConfigureIdentity(IServiceCollection services)
 {
@@ -70,12 +87,10 @@ void ConfigureIdentity(IServiceCollection services)
         options.Password.RequireUppercase = false;
         options.Password.RequiredLength = 8;
         options.Password.RequiredUniqueChars = 1;
-
         // Lockout settings
         options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
         options.Lockout.MaxFailedAccessAttempts = 3;
         options.Lockout.AllowedForNewUsers = true;
-
         // User settings
         options.User.RequireUniqueEmail = true;
     })
@@ -83,29 +98,39 @@ void ConfigureIdentity(IServiceCollection services)
     .AddDefaultTokenProviders();
 }
 
-// Seed the database
-using (var scope = app.Services.CreateScope())
+try
 {
-    var context = scope.ServiceProvider.GetRequiredService<DataContext>();
-    SeedData.SeedDatabase(context);
+    Log.Information("Starting web application");
+
+    // Seed the database
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<DataContext>();
+        SeedData.SeedDatabase(context);
+    }
+
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        try
+        {
+            var userManager = services.GetRequiredService<UserManager<AppUser>>();
+            var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            await AdminInitializer.InitializeAsync(userManager, roleManager);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occurred while seeding the database");
+        }
+    }
+
+    app.Run();
 }
-
-
-using (var scope = app.Services.CreateScope())
+catch (Exception ex)
 {
-    var services = scope.ServiceProvider;
-    try
-    {
-        var userManager = services.GetRequiredService<UserManager<AppUser>>();
-        var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-        await AdminInitializer.InitializeAsync(userManager, roleManager);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
-    }
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-app.Run();
-
+finally
+{
+    Log.CloseAndFlush();
+}
